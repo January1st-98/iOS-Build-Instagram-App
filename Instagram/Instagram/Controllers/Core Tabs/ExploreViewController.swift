d607f6c7721d14554cbfae8d0cc116484ec73f26
Jaehoon-So <jhspacelover@naver.com>
2022-05-08 21:53:10 +0900

    Project Setting
    1. 프로젝트를 MVC구조로 수행하기 위해서 그룹을 나누었습니다.
    2. 앞으로 개발하게 될 여러 기능들이 담길 Controller파일을 미리 작성하였습니다.
    3. Tab Bar를 사용해 띄워지게 될 스토리보드 뷰들을 추가하였습니다.

---
+++ Instagram/Instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ExplorViewController.swift
+//  Instagram
+//
+//  Created by Jaehoon So on 2022/05/08.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Jaehoon So on 2022/05/08.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
